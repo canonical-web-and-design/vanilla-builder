957e9bddf79a362e86af7ac216fcc96a08606d67
Robin Winslow <robin@robinwinslow.co.uk>
2015-04-29 17:00:13 +0100

    Compile CSS and upload to the assets server

--- publish.sh
+++ publish.sh
@@ -5,23 +5,26 @@ set -ex
 source lib/functions.sh
 
  # Check this version doesn't exist
-if [[ -z "${RELEASE_LEVEL}" ]]; then
-    echo "No RELEASE_LEVEL specified. Exiting."
+if [[ -z "${4}" ]]; then
+    echo 'Not all options specified.'
+    echo 'Usage: publish.sh ${FRAMEWORK_REPOSITORY} ${RELEASE_LEVEL} ${ASSETS_SERVER_URL} ${ASSETS_SERVER_TOKEN}'
     exit 1
 fi
 
 # Settings
+export framework_repository=$1
+export release_level=$2
+export assets_server_url=$3
+export assets_server_token=$4
 export FRAMEWORK_DIR=`pwd`/framework
 export HOMEPAGE_DIR=`pwd`/homepage
 export LIB_DIR=`pwd`/lib
-export ASSETS_URL=https://162.213.32.143/v1/
 
-prepare_directories git@github.com:nottrobin/vanilla-framework.git
-increment_npm_version ${RELEASE_LEVEL}
-new_version=$(increment_npm_version ${RELEASE_LEVEL})
+prepare_directories ${framework_repository}
+new_version=$(increment_npm_version ${release_level})
 #npm_publish
 add_version_tag ${new_version}
 compile_css
-upload_css
+upload_css ${new_version} ${assets_server_url} ${assets_server_token}
 update_docs
 update_project_homepage
